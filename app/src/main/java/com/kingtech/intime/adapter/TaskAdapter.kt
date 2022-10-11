033faee9a1aec1d664b2fc443f5f6e7b86bbbe65
Krisna Sirait <krisnapande00@outlook.com>
2022-10-11 23:29:12 +0700

    changes on TaskAdapter.kt

--- app/src/main/java/com/kingtech/intime/adapter/TaskAdapter.kt
+++ app/src/main/java/com/kingtech/intime/adapter/TaskAdapter.kt
@@ -1,34 +1,32 @@
 package com.kingtech.intime.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.kingtech.intime.R
 import com.kingtech.intime.data.Task
 import com.kingtech.intime.databinding.ItemContainerTaskBinding
+import kotlinx.android.synthetic.main.item_container_task.view.*
 
-class TaskAdapter(val task: List<Task>) : RecyclerView.Adapter<TaskAdapter.TodoViewHolder>() {
+class TaskAdapter(val listTask: List<Task>) : RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
-    inner class TodoViewHolder(private val binding: ItemContainerTaskBinding)
-        : RecyclerView.ViewHolder(binding.root) {
-            fun bind(taskModel: Task) {
-               binding.tvTitleTask.text = taskModel.title
-            }
-        }
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        return TodoViewHolder(ItemContainerTaskBinding.inflate(
-            LayoutInflater.from(parent.context),
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_container_task,
             parent,
-            false
-        ))
+            false)
+        return ViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        holder.bind(task[position])
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.itemView.tvTitleTask.text = listTask[position].title
     }
 
     override fun getItemCount(): Int {
-        return task.size
+        return listTask.size
     }
 
 
