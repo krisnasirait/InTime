60c3bb97b2a5cfe6e343693cd4be9a91ae980d8a
Krisna Sirait <krisnapande00@outlook.com>
2022-10-11 14:54:35 +0700

    created data class task

---
+++ app/src/main/java/com/kingtech/intime/data/TaskDatabase.kt
@@ -0,0 +1,12 @@
+package com.kingtech.intime.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(
+    entities = []
+)
+abstract class TaskDatabase: RoomDatabase() {
+
+}
