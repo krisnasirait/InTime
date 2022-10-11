02c56a80e2f5516cee4d91689155714970f327a0
Krisna Sirait <krisnapande00@outlook.com>
2022-10-11 23:17:08 +0700

    changes for class entity Task.kt

--- app/src/main/java/com/kingtech/intime/data/Task.kt
+++ app/src/main/java/com/kingtech/intime/data/Task.kt
@@ -1,6 +1,7 @@
 package com.kingtech.intime.data
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
@@ -8,9 +9,12 @@ import kotlinx.android.parcel.Parcelize
 
 
 @Entity
+@Parcelize
 data class Task(
     @PrimaryKey(autoGenerate = true)
-    var id: Long?,
+    var id: Int?,
+    @ColumnInfo(name = "title")
     var title: String?,
+    @ColumnInfo(name = "desc")
     var desc: String,
-)
+) : Parcelable
